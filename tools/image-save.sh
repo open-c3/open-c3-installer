c212688c125e3432ce02bc824e9d8a4c095f4e31
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-07-19 18:39:54 +0800

    保存镜像之前做一次拉取操作，确保镜像存在

--- tools/image-save.sh
+++ tools/image-save.sh
@@ -5,5 +5,8 @@ mkdir -p /data/open-c3-installer/dockerimage
 
 cd /data/open-c3-installer/dockerimage || exit 1
 
+cat ../open-c3/Installer/C3/JOB/dockerfile        | grep FROM  |               awk       '{print $2}' | awk '{print "docker pull    " $1       }'              | bash -x
 cat ../open-c3/Installer/C3/JOB/dockerfile        | grep FROM  |               awk       '{print $2}' | awk '{print "docker save -o " $1 " " $1}'|sed 's/\//_/'| bash -x
+
+cat /data/open-c3/Installer/C3/docker-compose.yml | grep image | grep -v "^#"| awk -F'"' '{print $2}' | awk '{print "docker pull    " $1       }'              | bash -x
 cat /data/open-c3/Installer/C3/docker-compose.yml | grep image | grep -v "^#"| awk -F'"' '{print $2}' | awk '{print "docker save -o " $1 " " $1}'|sed 's/\//_/'| bash -x
